3d6eac6ed64be0393e62bb8f99975f8b10222d1e
KoSeonJe <127813439+KoSeonJe@users.noreply.github.com>
2026-05-07 00:52:35 +0900

    test(BE): Service Integration cleanup 격리 보강

--- backend/src/test/java/com/rehearse/api/global/support/ServiceIntegrationSupport.java
+++ backend/src/test/java/com/rehearse/api/global/support/ServiceIntegrationSupport.java
@@ -1,5 +1,6 @@
 package com.rehearse.api.global.support;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +18,15 @@ public abstract class ServiceIntegrationSupport extends AbstractMySqlContainerTe
 
     @BeforeEach
     void truncateDatabase() {
+        truncateAllTables();
+    }
+
+    @AfterEach
+    void cleanDatabase() {
+        truncateAllTables();
+    }
+
+    private void truncateAllTables() {
         jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
         List<String> tables = jdbcTemplate.queryForList("""
                 SELECT table_name
